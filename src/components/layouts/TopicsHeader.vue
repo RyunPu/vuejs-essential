2bb8b170e0722024b28253ef178d75caaee8debb
Yuanmin Pu <ryunpu@163.com>
2018-03-06 20:24:32 +0800

    website logo

---
+++ src/components/layouts/TopicsHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopicsHeader',
+  data() {
+    return {
+      logo: {
+        src: `${this.uploadsUrl}sites/hG5JuDSqZ7Y26Kuh0Qat8EYv6XNT0fGc.png`,
+        title: 'Laravel China'
+      }
+    }
+  },
+  beforeCreate() {
+    this.uploadsUrl = 'https://lccdn.phphub.org/uploads/';
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title { display: none;}
+</style>
