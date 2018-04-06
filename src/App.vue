3cdfa4dcbc65706e99eb23a8f5e6d0f5c533e0e7
Yuanmin Pu <ryunpu@163.com>
2018-04-06 19:10:42 +0800

    bottom area

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="wrap">
     <TheHeader/>
+    <TheFooter/>
   </div>
 </template>
 
 <script>
 import TheHeader from '@/components/layouts/TheHeader'
+import TheFooter from '@/components/layouts/TheFooter'
 
 export default {
   name: 'App',
   components: {
-    TheHeader
+    TheHeader,
+    TheFooter
   }
 }
 </script>
