8c2ee573d662d7a4a71fe1239615ef86a3cbdd7c
Yuanmin Pu <ryunpu@163.com>
2018-04-27 13:36:29 +0800

    add search input

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,8 @@ Vue.use(Vuex)
 const state = {
   user: ls.getItem('user'),
   auth: ls.getItem('auth'),
-  articles: ls.getItem('articles')
+  articles: ls.getItem('articles'),
+  searchValue: ''
 }
 
 const mutations = {
@@ -25,6 +26,9 @@ const mutations = {
   UPDATE_ARTICLES(state, articles) {
     state.articles = articles
     ls.setItem('articles', articles)
+  },
+  UPDATE_SEARCH_VALUE(state, searchValue) {
+    state.searchValue = searchValue
   }
 }
 
