311cf4d61e2521382396904b7b88d975de48fedf
Yuanmin Pu <ryunpu@163.com>
2018-05-17 15:38:44 +0800

    navigation list

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -2,10 +2,25 @@
   <div class="navbar navbar-default topnav">
     <div class="container">
       <div class="navbar-header">
+        <button type="button" class="navbar-toggle" @click="toggleNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
         <a href="/" class="navbar-brand">
           <span class="title">{{ logo.title }}</span>
           <img :src="logo.src" :alt="logo.title">
         </a>
+      </div>
+
+      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -19,15 +34,27 @@ export default {
       logo: {
         src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
         title: 'VuejsCaff'
-      }
+      },
+      navList: ['社区', '头条', '问答', '教程'],
+      activeNavIndex: 0,
+      showCollapsedNav: false
     }
   },
   beforeCreate() {
     this.uploadsUrl = 'https://vuejscaffcdn.phphub.org/uploads/'
+  },
+  methods: {
+    changeNavIndex(index) {
+      this.activeNavIndex = index
+    },
+    toggleNav() {
+      this.showCollapsedNav = !this.showCollapsedNav
+    }
   }
 }
 </script>
 
 <style scoped>
 .title { display: none;}
+.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
 </style>
