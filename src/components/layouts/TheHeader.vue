eb19fb9975840898bce4b251f78486bf899d11f1
Yuanmin Pu <ryunpu@163.com>
2018-05-17 15:28:42 +0800

    website logo

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  data() {
+    return {
+      logo: {
+        src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
+        title: 'VuejsCaff'
+      }
+    }
+  },
+  beforeCreate() {
+    this.uploadsUrl = 'https://vuejscaffcdn.phphub.org/uploads/'
+  }
+}
+</script>
+
+<style scoped>
+.title { display: none;}
+</style>
