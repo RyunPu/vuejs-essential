ab343113bbed15c2aba48308bd98c8272ec7c7f2
Yuanmin Pu <ryunpu@163.com>
2018-05-17 14:27:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 200px;
+
+  h1 {
+    color: #42b983;
+  }
+}
+</style>
