0be2ecea42a2795fb9707a82039cc5a6fc65349c
Yuanmin Pu <ryunpu@163.com>
2018-05-19 20:46:33 +0800

    build for Github Pages

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -51,7 +51,7 @@ export default {
         src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
         title: 'VuejsCaff'
       },
-      navList: ['社区', '头条', '问答', '教程'],
+      navList: ['社区'],
       activeNavIndex: 0,
       showCollapsedNav: false
     }
